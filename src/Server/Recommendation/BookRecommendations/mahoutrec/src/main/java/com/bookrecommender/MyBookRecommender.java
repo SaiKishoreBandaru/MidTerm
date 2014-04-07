66eea2e466a21cb6dc327175369881c1949142e3
SaiKishoreBandaru <sbqm9@mail.umkc.edu>
2014-04-06 23:42:42 -0500

    app
    relaod changes

--- src/Server/Recommendation/BookRecommendations/mahoutrec/src/main/java/com/bookrecommender/MyBookRecommender.java
+++ src/Server/Recommendation/BookRecommendations/mahoutrec/src/main/java/com/bookrecommender/MyBookRecommender.java
@@ -1,48 +1,56 @@
-package com.unresyst;
+package com.bookrecommender;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import java.util.List;
+import java.util.Map;
 import org.apache.commons.cli2.OptionException; 
 import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.m.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.slopeone.SlopeOneRecommender;
-import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.m.DataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 
-public class UnresystBoolRecommend {
+public class MyBookRecommender {
     
     public static void main(String... args) throws FileNotFoundException, TasteException, IOException, OptionException {
         
-        // create data source (model) - from the csv file            
-        File ratingsFile = new File("datasets/dummy-bool.csv");                        
-        DataModel model = new FileDataModel(ratingsFile);
+        // create data source (m) - from the csv file            
+        File rate = new File("/home/cloudera/Books_Ratings.csv");                        
+        DataModel m = new FileDataModel(rate);
         
         // create a simple recommender on our data
-        CachingRecommender cachingRecommender = new CachingRecommender(new SlopeOneRecommender(model));
+        CachingRecommender cr = new CachingRecommender(new SlopeOneRecommender(m));
 
         // for all users
-        for (LongPrimitiveIterator it = model.getUserIDs(); it.hasNext();){
-            long userId = it.nextLong();
+        for (LongPrimitiveIterator it = m.getUserIDs(); it.hasNext();){
+            long id = it.nextLong();
             
             // get the recommendations for the user
-            List<RecommendedItem> recommendations = cachingRecommender.recommend(userId, 10);
+            List<RecommendedItem> recommendations = cr.recommend(id, 10);
             
             // if empty write something
             if (recommendations.size() == 0){
-                System.out.print("User ");
-                System.out.print(userId);
-                System.out.println(": no recommendations");
+                System.out.print("User ");System.out.print(id);System.out.println(": no recommendations");
             }
                             
             // print the list of recommendations for each 
             for (RecommendedItem recommendedItem : recommendations) {
                 System.out.print("User ");
-                System.out.print(userId);
+                System.out.print(id);
                 System.out.print(": ");
                 System.out.println(recommendedItem);
             }
